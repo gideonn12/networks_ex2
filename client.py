d5851eb8926b7aefb039c7d35593474d10d42c77
gideonn12 <155386445+gideonn12@users.noreply.github.com>
2024-12-12 19:04:37 +0200

    server works

--- client.py
+++ client.py
@@ -1,7 +1,5 @@
 import socket, sys
 
-from server import BUFFER_SIZE
-
 TCP_IP = sys.argv[1]
 TCP_PORT = int(sys.argv[2])
 BUFFER_SIZE = 1024
@@ -10,8 +8,31 @@ BUFFER_SIZE = 1024
 def format_msg(path):
     return f"GET" + {path} + "HTTP/1.1\nConnection: keep-alive\r\n\r\n"
 
-def read_message(socket):
+
+def read_message(s):
+    msg = ''
+    while True:
+        data = s.recv(BUFFER_SIZE)
+        if not data:
+            break
+        msg += data.decode()
+    return msg
+
+
+def message_to_file(message, file_name):
+    lines = message.splitlines()
+    if "200 OK" in lines[0]:
+        empty_line_index = lines.index("")
+        data = "\n".join(lines[empty_line_index + 1:])
+        try:
+            data.encode('utf-8')
+            with open(file_name, "w") as f:
+                f.write(data)
+        except UnicodeEncodeError:
+            with open(file_name, "wb") as f:
+                f.write(message.encode('utf-8'))
     return
+
 
 # if the response
 
@@ -20,9 +41,12 @@ while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
     while True:
-    # get path from the CLI
-    path = input("\n")
-    # something going to break here
-    s.send(format_msg(path).encode())
-    data = s.recv(BUFFER_SIZE)
+        # get path from the CLI
+        path = input("\n")
+        # something going to break here
+        s.send(format_msg(path).encode())
+        read_message(s)
+
+        break
+    break
 s.close()
--- server.py
+++ server.py
@@ -3,9 +3,9 @@ from os import close
 
 import socket, sys
 
-#TCP_IP = '127.0.0.1'
-TCP_PORT = int(sys.argv[1])
-TCP_PORT = 12345
+# TCP_IP = '127.0.0.1'
+#TCP_PORT = int(sys.argv[1])
+TCP_PORT = 12346
 BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,12 +16,14 @@ s.listen(0)
 # a function to search for the file
 # if the file exists, ret true, else ret false
 def search_file(file_name):
+    file_name = file_name[1:]
     file_path = os.path.join('files', file_name)
     return os.path.exists(file_path)
 
 
 def read_file(file_name):
     # Construct the full path to the file
+    file_name = file_name[1:]
     file_path = os.path.join('files', file_name)
     if file_name.endswith('.jpg') or file_name.endswith('.ico'):
         with open(file_path, 'rb') as f:
@@ -50,7 +52,6 @@ def read_message(connection):
     while True:
         try:
             data += (connection.recv(BUFFER_SIZE)).decode()
-            print("received:", data)
             if not data:
                 connection.close()
                 return None
@@ -64,40 +65,40 @@ def read_message(connection):
             connection.close()
             return None
 
+
 def return_message(connection, connectionType, file):
     http_response = 'HTTP/1.1'
     conn_response = 'Connection: '
     length_response = 'Content-Length: '
     location_response = 'Location: '
 
-    if file == "result.html":
+    if file == "/redirect":
         http_response += ' 301 Found\n'
         conn_response += 'close\n'
         location_response += "/result.html\n"
         response = http_response + conn_response + location_response + '\n'
         connection.send(response.encode())
-        return  # Return early for this condition
+        return False
 
     if search_file(file):
-        print("file found")
         http_response += ' 200 OK\n'
         conn_response += connectionType + '\n'
         file_content = read_file(file)
         length_response += str(len(file_content)) + '\n\n'
         response = http_response + conn_response + length_response + file_content + '\n'
         connection.send(response.encode())
-        print("res sent")
+        return True
     else:
         http_response += ' 404 Not Found\n'
         conn_response += 'close\n'
         response = http_response + conn_response + '\n'
         connection.send(response.encode())
-
+        connection.close()
+        return False
 
 
 while True:
     conn, addr = s.accept()
-    print('New connection from:', addr)
     while True:
         message_from_client = read_message(conn)
         if not message_from_client:
@@ -108,11 +109,7 @@ while True:
 
         file, connectionType = decipher_message(message_from_client)
         if file == "/":
-            file = "index.html"
-        elif file == "/redirect":
-            file = "result.html"
-        print("file:", file)
-        return_message(conn,connectionType,file)
-        if connectionType == 'close':
+            file = "/index.html"
+        if not return_message(conn, connectionType, file):
             break
     conn.close()
