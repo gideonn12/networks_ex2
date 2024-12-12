8f5a3589a7953f3065aa5fa34d42060e23dba57b
gideonn12 <155386445+gideonn12@users.noreply.github.com>
2024-12-12 12:35:51 +0200

    nearly finished on server

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 from os import close
-import socket,sys
+
+import socket, sys
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = int(sys.argv[1])
@@ -14,30 +15,20 @@ s.listen(0)
 # a function to search for the file
 # if the file exists, ret true, else ret false
 def search_file(file_name):
-    try:
-        f = open(file_name, 'r')
-        f.close()
-        return True
-    except:
-        return False
+    file_path = os.path.join('files', file_name)
+    return os.path.exists(file_path)
+
 
 def read_file(file_name):
     # Construct the full path to the file
     file_path = os.path.join('files', file_name)
+    if file_name.endswith('.jpg') or file_name.endswith('.ico'):
+        with open(file_path, 'rb') as f:
+            return f.read()
+    else:
+        with open(file_path, 'r') as f:
+            return f.read()
 
-    try:
-        with open(file_path, 'r') as file:
-            content = file.read()
-        return content
-    except FileNotFoundError:
-        return f"File {file_name} not found."
-    except Exception as e:
-        return str(e)
-
-
-
-def return_message():
-    return
 
 def decipher_message(message):
     lines = message.split('\n')
@@ -49,6 +40,7 @@ def decipher_message(message):
         elif line.startswith('Connection'):
             connection_line = line.split(': ')[1]  # Extract text from Connection till the end
     return get_line, connection_line
+
 
 def read_message(connection):
     data = ''
@@ -69,9 +61,31 @@ def read_message(connection):
             close(connection)
             return None
 
-def print_message(msg):
 
+def return_message(connection, file):
+    http_response = 'HTTP/1.1'
+    conn_response = 'Connection: '
+    length_response = 'Content-Length: '
+    location_response = 'Location: '
+
+    if file == "result.html":
+        http_response += ' 301 Found\n'
+        conn_response += 'close\n'
+        location_response += "/result.html\n"
+        connection.send(http_response + conn_response + location_response + '\n')
+    if search_file(file):
+        http_response += ' 200 OK\n'
+        conn_response += connection + '\n'
+        # TODO: check if hello world is 11 as should be
+        length_response += str(len(read_file(file))) + '\n\n'
+        connection.send(http_response + conn_response + length_response + '\n' + read_file(file) + '\n')
+    else:
+        http_response += ' 404 Not Found\n'
+        conn_response += 'close\n'
+        connection.send(http_response + conn_response + '\n')
+        return
     return
+
 
 while True:
     conn, addr = s.accept()
@@ -80,6 +94,16 @@ while True:
         message_from_client = read_message(conn)
         if not message_from_client:
             break
-        file,connection = decipher_message(message_from_client)
 
+        # print the msg from the client to the screen
+        print(message_from_client)
+
+        file, connection = decipher_message(message_from_client)
+        if file == "/":
+            file = "index.html"
+        elif file == "/redirect":
+            file = "result.html"
+        return_message(conn, file)
+        if connection == 'close':
+            break
     conn.close()
